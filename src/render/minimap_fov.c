d1d5ddd168c8dd443cb15adbe0e1d6e493b9f9f9
Bernhard Biermann <bbierman@c3a1c2.42berlin.de>
2025-05-09 14:23:00 +0200

    minimap up&runnin; need to fix the scaling-issue with big maps

--- src/render/minimap_fov.c
+++ src/render/minimap_fov.c
@@ -6,7 +6,7 @@
 /*   By: bbierman <bbierman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 10:55:13 by bbierman          #+#    #+#             */
-/*   Updated: 2025/05/09 12:41:44 by bbierman         ###   ########.fr       */
+/*   Updated: 2025/05/09 13:54:22 by bbierman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ void	draw_minimap_fov(t_data *game)
 void	draw_minimap_ray(t_data *game, double rel_angle)
 {
 	double	dir_angle;
-	double	ray_dir_x;
-	double	ray_dir_y;
+	double	start_x;
+	double	start_y;
 	double	len;
 	int		px;
 	int		py;
@@ -42,17 +42,15 @@ void	draw_minimap_ray(t_data *game, double rel_angle)
 
 	i = 0;
 	dir_angle = atan2(game->player.dir_y, game->player.dir_x) + rel_angle * (PI / 180.0);
-	ray_dir_x = cos(dir_angle);
-	ray_dir_y = sin(dir_angle);
 	len = calculate_wall_distance(game, dir_angle, &game->player.orientation, true);
 	if (len > MINI_RAY_LENGTH)
 		len = MINI_RAY_LENGTH;
-	double	start_x = MAP_OFFSET_X + game->player.x * MINI_TILE_SIZE;
-	double	start_y = MAP_OFFSET_Y + game->player.y * MINI_TILE_SIZE;
+	start_x = MAP_OFFSET_X + game->player.x * MINI_TILE_SIZE;
+	start_y = MAP_OFFSET_Y + game->player.y * MINI_TILE_SIZE;
 	while (i < len * MINI_TILE_SIZE)
 	{
-		px = start_x + ray_dir_x * i;
-		py = start_y + ray_dir_y * i;
+		px = start_x + cos(dir_angle) * i;
+		py = start_y + sin(dir_angle) * i;
 		put_pixel_to_image(game, px, py, 0x00FF00);
 		i++;
 	}
